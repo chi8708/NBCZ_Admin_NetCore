fac4d3c643f538ac31f15ac056a7264bc64f68b8
chi8708 <983143462@qq.com>
2019-08-02 16:57:20 +0800

    用户信息crud

---
+++ NBCZ.Api/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NBCZ.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Base")]
+    public class BaseController : Controller
+    {
+    }
+}
